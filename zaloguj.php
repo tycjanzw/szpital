d194d702c7ddf5dda71d7806ea6fc9d96e1018c9
tycjanzw <58362230+tycjanzw@users.noreply.github.com>
2020-05-30 18:46:57 +0200

    Add files via upload

---
+++ zaloguj.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+require_once "connect.php";
+
+$polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+if($polaczenie->connect_errno!=0)
+{
+    echo "Error.: ".$polaczenie->connect_errno;
+}
+else{
+    $login= $_POST['login'];
+    $haslo = $_POST['haslo'];
+    $wybor = $_POST['wybor'];
+   
+
+    
+   
+    if($wybor == '1'){
+    
+        $sql = " SELECT * FROM klient WHERE login ='$login' AND haslo ='$haslo'; ";
+
+    if ($rezultat = @$polaczenie->query($sql))
+    {
+        $ilu_user = $rezultat->num_rows;
+        if($ilu_user>0) 
+        
+        {
+            
+            $wiersz = $rezultat->fetch_assoc();
+            $_SESSION['login'] = $wiersz['login'];
+            $_SESSION['pesel'] = $wiersz['pesel'];
+
+            $rezultat->free_result();
+
+           header('location: panel_klient.php');
+
+        }else{
+            $_SESSION['blad']='<span style="color:red">Nieprawidlowy login lub haslo!</span>';
+            header('location: log.php
+            ');
+
+        }
+    }
+ }
+
+ if($wybor == '2'){
+    
+    $sql = " SELECT * FROM asystent WHERE login ='$login' AND haslo ='$haslo'; ";
+
+if ($rezultat = @$polaczenie->query($sql))
+{
+    $ilu_user = $rezultat->num_rows;
+    if($ilu_user>0) 
+    
+    {
+        
+        $wiersz = $rezultat->fetch_assoc();
+        $_SESSION['login'] = $wiersz['login'];
+        $_SESSION['pesel'] = $wiersz['pesel'];
+
+        $rezultat->free_result();
+
+       header('location: panel_asystent.php');
+
+    }else{
+        $_SESSION['blad']='<span style="color:red">Nieprawidlowy login lub haslo!</span>';
+        header('location: log.php
+        ');
+
+    }
+}
+}
+
+if($wybor == '3'){
+    
+    $sql = " SELECT * FROM weterynarz WHERE login ='$login' AND haslo ='$haslo'; ";
+
+if ($rezultat = @$polaczenie->query($sql))
+{
+    $ilu_user = $rezultat->num_rows;
+    if($ilu_user>0) 
+    
+    {
+        
+        $wiersz = $rezultat->fetch_assoc();
+        $_SESSION['login'] = $wiersz['login'];
+        $_SESSION['pesel'] = $wiersz['pesel'];
+
+        $rezultat->free_result();
+
+       header('location: panel_weterynarz.php');
+
+    }else{
+        $_SESSION['blad']='<span style="color:red">Nieprawidlowy login lub haslo!</span>';
+        header('location: log.php
+        ');
+
+    }
+}
+}
+    
+    $polaczenie->close();
+}
+
+
+
+
+
+
+
+?>
